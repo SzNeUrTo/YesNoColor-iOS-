c339a8c6eaa2f6ed49c65e6549f51f30cbd6cd13
Supanut Apikulvanich <Supanut@Supanuts-MacBook-Air.local>
2015-06-13 19:33:23 +0700

    Initial Commit

---
+++ YesNoColorUITests/YesNoColorUITests.swift
@@ -0,0 +1,35 @@
+//
+//  YesNoColorUITests.swift
+//  YesNoColorUITests
+//
+//  Created by Supanut Apikulvanich on 6/13/2558 BE.
+//  Copyright © 2558 Supanut Apikulvanich. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class YesNoColorUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
